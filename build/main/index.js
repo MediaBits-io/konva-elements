8f17340b012a83205410999e705256dfbd1f9592
Vincas Stonys <vincaslt@gmail.com>
2020-11-22 23:00:42 +0200

    Update package versions

--- build/main/index.js
+++ build/main/index.js
@@ -1,7 +1,11 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.ProgressBar = exports.Waveform = void 0;
 var Waveform_1 = require("./Waveform/Waveform");
-Object.defineProperty(exports, "Waveform", { enumerable: true, get: function () { return Waveform_1.default; } });
+Object.defineProperty(exports, "Waveform", { enumerable: true, get: function () { return __importDefault(Waveform_1).default; } });
 var ProgressBar_1 = require("./ProgressBar/ProgressBar");
-Object.defineProperty(exports, "ProgressBar", { enumerable: true, get: function () { return ProgressBar_1.default; } });
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi8uLi9zcmMvaW5kZXgudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7QUFBQSxnREFJNkI7QUFIM0Isb0dBQUEsT0FBTyxPQUFZO0FBSXJCLHlEQUdtQztBQUZqQywwR0FBQSxPQUFPLE9BQWUifQ==
+Object.defineProperty(exports, "ProgressBar", { enumerable: true, get: function () { return __importDefault(ProgressBar_1).default; } });
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi8uLi9zcmMvaW5kZXgudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7Ozs7O0FBQUEsZ0RBSTZCO0FBSDNCLHFIQUFBLE9BQU8sT0FBWTtBQUlyQix5REFHbUM7QUFGakMsMkhBQUEsT0FBTyxPQUFlIn0=
--- build/module/Waveform/patterns/utils.js
+++ build/module/Waveform/patterns/utils.js
@@ -1,1 +1,2 @@
+export {};
 //# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoidXRpbHMuanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi8uLi8uLi8uLi9zcmMvV2F2ZWZvcm0vcGF0dGVybnMvdXRpbHMudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiJ9
